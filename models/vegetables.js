117fde92950ba9056f308679f824695057379964
venupriya1 <priyanikavenu@gmail.com>
2023-12-07 13:13:37 -0800

    fruits and vegetables

---
+++ models/vegetables.js
@@ -0,0 +1,19 @@
+const vegetables = [
+    {
+        name:'raddish',
+        color: 'red',
+        readyToEat: true
+    },
+    {
+        name:'carrots',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'cucumber',
+        color: 'yellow',
+        readyToEat: true
+    }
+];
+
+module.exports = vegetables;
